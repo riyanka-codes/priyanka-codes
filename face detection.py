90c957edc007e879b369889dd16ff1883350298f
RajaPriyanka <115486959+riyanka-codes@users.noreply.github.com>
2022-10-31 20:38:22 +0530

    Using Python Libraries

---
+++ face detection.py
@@ -0,0 +1,19 @@
+import cv2
+alg = "haarcascade_frontalface_default.xml" #accessed the model file
+haar_cascade = cv2.CascadeClassifier(alg)
+
+cam = cv2.VideoCapture(0)  #initializing camera
+
+while True:
+    _,img = cam.read()  #read the frame from the camera
+    grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face = haar_cascade.detectMultiScale(grayImg,1.3,4) #get coordinates of face
+
+    for (x,y,w,h) in face:
+        cv2.rectangle(img,(x,y),(x+w,y+h), (0,255,0), 2)
+    cv2.imshow("Face detection",img)
+    key = cv2.waitKey(10)
+    if key == ord("k"):
+        break
+cam.release()
+cv2.destroyAllWindows()
